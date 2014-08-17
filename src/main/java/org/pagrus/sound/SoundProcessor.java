a515d613fbe35a2e289d241dc05848ebb5640ad6
Pavel Grushetzky <pagrus@gmail.com>
2014-08-17 23:56:33 +0300

    Flanger - initial setup

--- src/main/java/org/pagrus/sound/SoundProcessor.java
+++ src/main/java/org/pagrus/sound/SoundProcessor.java
@@ -3,12 +3,12 @@ package org.pagrus.sound;
 import java.util.Arrays;
 
 import org.pagrus.sound.effects.Amplifier;
-import org.pagrus.sound.effects.Delay;
+import org.pagrus.sound.effects.Flanger;
 import org.pagrus.sound.plumbing.StereoOut;
 
 public class SoundProcessor {
-  private Delay delay = new Delay(200, 1, 0.5);
   private Amplifier amp = new Amplifier(2);
+  private Flanger flanger = new Flanger(3, 1, 1, 1, 0.5);
 
   /**
    * Process a single buffer of sound samples and write results to<code>out</code>. 
@@ -17,11 +17,12 @@ public class SoundProcessor {
    */
   public void processBuffer(int[] inputSamples, StereoOut out, long sampleTime, long estimatedSampleTimeNanos) {
 
+    // TODO - complete the implementation of flanger
     Arrays.stream(inputSamples)
     .mapToDouble(i -> ((double) i / Integer.MAX_VALUE))
 
     .map(amp::apply)
-    .map(delay::apply)
+    .map(flanger::apply)
 
     .mapToInt(d -> ((int)(d* Integer.MAX_VALUE)))
     .forEach(i -> out.putInt(i));
