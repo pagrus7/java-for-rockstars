f471c402d64ba9f8ebb47e4c2d2e97d5f2eca1bb
Pavel Grushetzky <pagrus@gmail.com>
2014-08-22 18:08:37 +0300

    Flanger done

--- src/main/java/org/pagrus/sound/SoundProcessor.java
+++ src/main/java/org/pagrus/sound/SoundProcessor.java
@@ -4,11 +4,14 @@ import java.util.Arrays;
 
 import org.pagrus.sound.effects.Amplifier;
 import org.pagrus.sound.effects.Flanger;
+import org.pagrus.sound.effects.SoundFileReader;
+import org.pagrus.sound.effects.SoundMixer;
 import org.pagrus.sound.plumbing.StereoOut;
 
 public class SoundProcessor {
   private Amplifier amp = new Amplifier(2);
-  private Flanger flanger = new Flanger(3, 1, 1, 1, 0.5);
+  private Flanger flanger = new Flanger(4, 1, 2, 1, 0.5);
+  private SoundMixer track = new SoundMixer(1, 1, SoundFileReader.INSTANCE.readAsArray("/my/music/collection/come-as-you-are-E-fragment.mp3"));
 
   /**
    * Process a single buffer of sound samples and write results to<code>out</code>. 
@@ -17,12 +20,12 @@ public class SoundProcessor {
    */
   public void processBuffer(int[] inputSamples, StereoOut out, long sampleTime, long estimatedSampleTimeNanos) {
 
-    // TODO - complete the implementation of flanger
     Arrays.stream(inputSamples)
     .mapToDouble(i -> ((double) i / Integer.MAX_VALUE))
 
     .map(amp::apply)
     .map(flanger::apply)
+    .map(track::mix)
 
     .mapToInt(d -> ((int)(d * Integer.MAX_VALUE)))
     .forEach(out::putInt);
