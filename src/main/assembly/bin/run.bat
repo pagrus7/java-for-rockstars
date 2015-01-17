3d50fef6fe1df81e6fa221e9562156684776efc3
Pavel Grushetzky <pagrus@gmail.com>
2015-01-18 00:03:46 +0300

    Backing track - started

--- src/main/assembly/bin/run.bat
+++ src/main/assembly/bin/run.bat
@@ -4,6 +4,6 @@ FOR %%F IN (lib/*.jar) DO (
   SET LIBS=!LIBS!;lib/%%F%
 )
 
-start java -cp bin;%LIBS% -verbose:gc -Djava.library.path=lib -Xms64m -Xmx64m -XX:+HeapDumpOnOutOfMemoryError org.pagrus.sound.ConsoleMain
+start java -cp bin;%LIBS% -verbose:gc -Djava.library.path=lib -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError org.pagrus.sound.ConsoleMain
 
 wmic process where name="java.exe" CALL setpriority 256
