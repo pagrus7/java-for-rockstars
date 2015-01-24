65484100a0ab76e469033e01b110e49232780d6a
Pavel Grushetzky <pagrus@gmail.com>
2015-01-24 22:19:34 +0300

    Flanger - started

--- src/main/java/org/pagrus/sound/SoundProcessor.java
+++ src/main/java/org/pagrus/sound/SoundProcessor.java
@@ -3,17 +3,12 @@ package org.pagrus.sound;
 import java.util.Arrays;
 
 import org.pagrus.sound.effects.Amplifier;
-import org.pagrus.sound.effects.Delay;
-import org.pagrus.sound.effects.SoundFileReader;
-import org.pagrus.sound.effects.SoundMixer;
+import org.pagrus.sound.effects.Flanger;
 import org.pagrus.sound.plumbing.StereoOut;
 
 public class SoundProcessor {
-
-  private SoundMixer track = new SoundMixer(1.0, 0.4, SoundFileReader.INSTANCE.readAsArray("/my/music/collection/lily-was-here-fragment.mp3"));
-  private Delay shortDelay = new Delay(25, 1, 0.5);
-  private Delay longDelay = new Delay(300, 1, 0.3);
   private Amplifier amp = new Amplifier(2);
+  private Flanger flanger = new Flanger(3, 1, 1, 1, 0.5);
 
   /**
    * Process a single buffer of sound samples and write results to<code>out</code>. 
@@ -22,13 +17,12 @@ public class SoundProcessor {
    */
   public void processBuffer(int[] inputSamples, StereoOut out, long sampleTime, long estimatedSampleTimeNanos) {
 
+    // TODO - complete the implementation of flanger
     Arrays.stream(inputSamples)
     .mapToDouble(i -> ((double) i / Integer.MAX_VALUE))
 
     .map(amp::apply)
-    .map(shortDelay::apply)
-    .map(longDelay::apply)
-    .map(track::mix)
+    .map(flanger::apply)
 
     .mapToInt(d -> ((int)(d * Integer.MAX_VALUE)))
     .forEach(out::putInt);
