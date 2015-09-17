438d5eae941648f7b88a79a280607dd837de2f39
Pavel Grushetzky <pagrus@gmail.com>
2015-09-17 23:30:10 +0300

    Flanger - started

--- src/main/java/org/pagrus/sound/SoundProcessor.java
+++ src/main/java/org/pagrus/sound/SoundProcessor.java
@@ -4,21 +4,15 @@ import java.util.function.Consumer;
 import java.util.stream.DoubleStream;
 
 import org.pagrus.sound.effects.Amplifier;
-import org.pagrus.sound.effects.Delay;
-import org.pagrus.sound.effects.SoundFileReader;
-import org.pagrus.sound.effects.SoundMixer;
+import org.pagrus.sound.effects.Flanger;
 import org.pagrus.sound.plumbing.StereoOut;
 
 public class SoundProcessor {
   private static final int DEFAULT_BUFFER_SIZE = 512;
   private Consumer<double[]> sniffer = d -> {};
 
-  private SoundMixer track = new SoundMixer(1.0, 0.4, 
-      SoundFileReader.INSTANCE.readAsArray(System.getenv("HOME") + "/personal/music/collection/lily-was-here-fragment.mp3"));
-
-  private Delay shortDelay = new Delay(25, 1, 0.5);
-  private Delay longDelay = new Delay(300, 1, 0.3);
-  private Amplifier amp = new Amplifier(3);
+  private Amplifier amp = new Amplifier(2);
+  private Flanger flanger = new Flanger(3, 1, 1, 1, 0.5);
 
   public SoundProcessor() {
     updateBufferSize(DEFAULT_BUFFER_SIZE);
@@ -37,9 +31,7 @@ public class SoundProcessor {
   public void processBuffer(DoubleStream input, StereoOut out, long sampleTime) {
     input
       .map(amp::amplify)
-      .map(shortDelay::apply)
-      .map(longDelay::apply)
-      .map(track::mix)
+      .map(flanger::apply)
       .forEach(out::putSample);
     }
 
--- src/main/java/org/pagrus/sound/effects/Flanger.java
+++ src/main/java/org/pagrus/sound/effects/Flanger.java
@@ -1,5 +1,46 @@
 package org.pagrus.sound.effects;
 
+import static java.lang.Math.PI;
+
 public class Flanger {
-  // coming soon
+  private static final int SAMPLE_RATE = 44100;
+  private final int bufferSize;
+  final double[] history;
+  private final int offsetDelta;
+  private final int offsetBase;
+  private final double originalSignalLevel;
+  private final double flangerSignalLevel;
+  private final double oscillationStep;
+
+  int idx;
+
+  public Flanger(double flangerOffsetMillis, double flangerRangeMillis, double freq, double originalSignalLevel, double flangerSignalLevel) {
+    this.originalSignalLevel = originalSignalLevel;
+    this.flangerSignalLevel = flangerSignalLevel;
+    this.offsetBase = (int) (flangerOffsetMillis * SAMPLE_RATE / 1000);
+    this.offsetDelta = (int) (flangerRangeMillis * SAMPLE_RATE / 1000);
+    this.oscillationStep = (2 * PI * freq) / SAMPLE_RATE;
+
+    bufferSize = offsetBase + offsetDelta;
+    history = new double[bufferSize];
+    idx = bufferSize - 1;
+  }
+
+
+  public double apply(double x) {
+    // Here. Calculate the historical index, or "how far back the sound to mix with should be picked".
+
+    // Flanger is similar to delay, but the amount of delay is variable: 
+    // - there is a fixed component, "offsetBase"
+    // - and a variable component, which fluctuates up and down between [-offsetDelta, offsetDelta] with a frequency "freq"
+    // the "oscillationStep" is a conveniently precalculated value: 
+    // it indicates how much the sinus argument needs to change for each new sample
+
+    int historicalIdx = (int) (0);  // TODO
+
+    x = x * originalSignalLevel + history[historicalIdx % bufferSize] * flangerSignalLevel;
+    history[idx++ % bufferSize] = x;
+    return x;
+  }
+
 }
