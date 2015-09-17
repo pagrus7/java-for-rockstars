5a6a601fe5eb4364b2c0bc0a8edd73649331337a
Pavel Grushetzky <pagrus@gmail.com>
2015-09-17 22:30:42 +0300

    Delay - started
    Check out SoundProcessor and Delay

--- src/main/java/org/pagrus/sound/SoundProcessor.java
+++ src/main/java/org/pagrus/sound/SoundProcessor.java
@@ -3,18 +3,14 @@ package org.pagrus.sound;
 import java.util.function.Consumer;
 import java.util.stream.DoubleStream;
 
-import org.pagrus.sound.effects.SoundFileReader;
-import org.pagrus.sound.effects.SoundMixer;
+import org.pagrus.sound.effects.Delay;
 import org.pagrus.sound.plumbing.StereoOut;
 
 public class SoundProcessor {
   private static final int DEFAULT_BUFFER_SIZE = 512;
   private Consumer<double[]> sniffer = d -> {};
 
-
-  private SoundMixer track = new SoundMixer(1.0, 2.5,
-      SoundFileReader.INSTANCE.readAsArray(System.getenv("HOME") + "/personal/music/collection/personal-jesus-E-fragment.mp3"));
-
+  private Delay delay = new Delay(200, 1, 0.5); ;
 
   public SoundProcessor() {
     updateBufferSize(DEFAULT_BUFFER_SIZE);
@@ -33,7 +29,7 @@ public class SoundProcessor {
   public void processBuffer(DoubleStream input, StereoOut out, long sampleTime) {
     input
       .map(d -> d * 2)
-      .map(d -> track.mix(d))
+      // TODO: apply delay
       .forEach(d -> out.putSample(d));
     }
 
