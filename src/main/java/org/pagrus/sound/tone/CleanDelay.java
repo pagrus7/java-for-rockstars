32afcc7cddd6f3ebef5314463effc96066406c6e
Pavel Grushetzky <pagrus@gmail.com>
2016-09-30 10:52:40 +0300

    More accented delay

--- src/main/java/org/pagrus/sound/tone/CleanDelay.java
+++ src/main/java/org/pagrus/sound/tone/CleanDelay.java
@@ -8,7 +8,7 @@ import java.util.stream.DoubleStream;
 public class CleanDelay implements Tone {
 
     Amplifier preamp = new Amplifier(7);
-    Delay delay = new Delay(30, 1d, 0.5);
+    Delay delay = new Delay(200, 1d, 0.5);
 
     @Override
     public DoubleStream with(DoubleStream input) {
--- src/main/java/org/pagrus/sound/tone/SlightDistortionWithDelay.java
+++ src/main/java/org/pagrus/sound/tone/SlightDistortionWithDelay.java
@@ -11,7 +11,7 @@ public class SlightDistortionWithDelay implements Tone {
   Amplifier preamp = new Amplifier(7);
   ClippingDistorion distorion = new ClippingDistorion(0.2, 0.05, 1);
 
-  Delay delay = new Delay(30, 1d, 0.5);
+  Delay delay = new Delay(200, 1d, 0.5);
   Amplifier postamp = new Amplifier(2);
 
   @Override
